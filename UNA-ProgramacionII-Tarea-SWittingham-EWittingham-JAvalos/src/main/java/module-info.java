ee6f439bb77cd49e7d976df5a32bc86062906dfa
110000110chan <stephany.wittingham.castrellon@est.una.ac.cr>
2026-03-07 11:08:15 -0600

    Configuración proyecto
    Se hace la configuración inicial del proyecto en Netbeans (estructura de paquetes, se crea la vista del administrador y se incorpora la hoja de estilos=

--- UNA-ProgramacionII-Tarea-SWittingham-EWittingham-JAvalos/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module cr.ac.una.una.programacionii.tarea.swittingham.ewittingham.javalos {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens cr.ac.una.una.programacionii.tarea.swittingham.ewittingham.javalos to javafx.fxml;
-    exports cr.ac.una.una.programacionii.tarea.swittingham.ewittingham.javalos;
-}

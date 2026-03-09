32eab4f7decedb2e74bbd92cf3244ced4b18a65c
Fabricio-Wittingham <eugenio.wittingham.castrellon@est.una.ac.cr>
2026-03-09 17:10:10 -0600

    Files deletion and inclusion of BranchesAndStationsManagement
    Removed unnecesary files from the repository and inclusion of StationsManagement view and controller for future configuration.

--- UNA-ProgramacionII-Tarea-SWittingham-EWittingham-JAvalos/src/main/java/cr/ac/una/una/programacionii/tarea/swittingham/ewittingham/javalos/App.java
+++ UNA-ProgramacionII-Tarea-SWittingham-EWittingham-JAvalos/src/main/java/cr/ac/una/una/programacionii/tarea/swittingham/ewittingham/javalos/App.java
@@ -12,9 +12,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import static javafx.application.Application.launch;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
---
+++ UNA-ProgramacionII-Tarea-SWittingham-EWittingham-JAvalos/src/main/java/cr/ac/una/una/programacionii/tarea/swittingham/ewittingham/javalos/controller/StationsManagementController.java
@@ -0,0 +1,12 @@
+package cr.ac.una.una.programacionii.tarea.swittingham.ewittingham.javalos.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class StationsManagementController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+}

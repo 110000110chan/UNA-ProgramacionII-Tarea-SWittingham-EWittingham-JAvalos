b2a4805c8193316821facebe7fe840407e192e85
JohanAvalosCampos <johanavalosuniversidad@gmail.com>
2026-03-13 16:00:59 -0600

    Aqui agruegue mi TramitesView e incorpore el tramitesController

---
+++ UNA-ProgramacionII-Tarea-SWittingham-EWittingham-JAvalos/src/main/java/cr/ac/una/tareaprograii2026/controller/TramitesController.java
@@ -0,0 +1,39 @@
+package cr.ac.una.tareaprograii2026.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class TramitesController extends Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        setNombreVista("Tramites");
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @FXML
+    private void onGuardar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onBuscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onEditar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onEliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActivo(ActionEvent event) {
+    }
+}
